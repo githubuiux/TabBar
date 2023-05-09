79adc1e0347363546b9eb3c4f1e036bde97b2454
githubuiux <uiuxista@gmail.com>
2023-05-09 20:13:05 +0300

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Levent on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
